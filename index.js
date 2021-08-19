179d5e033915602897d841ec5f174b9256502424
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-08-19 23:29:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const board = document.getElementById('board');
+const instruction = document.getElementById('instruction')
+
 window.addEventListener('load', (event) => {
     scanner.classList.add('animate-radar')
     document.body.style.cursor = "wait";
+    instruction.style.transition = "all 0.5s"
+    instruction.style.color = "#191f22"
 });
 
-let board = document.getElementById('board');
 
 board.addEventListener("animationend",  transitionAnim)
 
@@ -12,6 +16,8 @@ function transitionAnim(){
         board.style.display = "none"
         attribution.style.color = "#fff"
         attribution.style.transition = "all 2s"
+        instruction.style.color = "#fff"
+        instruction.style.transition = "all 2s"
         document.body.style.cursor = "default"
     },2800)    
 }
