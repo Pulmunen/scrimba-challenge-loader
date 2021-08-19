d81a6e61f483664ddd5ff3e4fa6ac257c59b23ed
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-08-19 21:11:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// const board = document.getElementById('board')
+// const boardSpokes = document.querySelector('board-spokes')
+// for (i=1; i <= 360; i++){
+//   board.innerHTML += `
+//   <div class="board-spokes" style="transform : rotate(${[i]}deg);" ></div>
+//   `}
