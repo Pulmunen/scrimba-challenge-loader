d0e0c99fda54982f298cd16a3fa4659d3f4f7f88
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-08-19 23:02:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 window.addEventListener('load', (event) => {
     scanner.classList.add('animate-radar')
+    document.body.style.cursor = "wait";
 });
+
+let board = document.getElementById('board');
+
+board.addEventListener("animationend",  transitionAnim)
+
+function transitionAnim(){
+    setTimeout(()=>{
+        board.style.display = "none"
+        attribution.style.color = "#fff"
+        attribution.style.transition = "all 2s"
+        document.body.style.cursor = "default"
+    },2800)    
+}
