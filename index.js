4431d082f45d51d42e4c8b73016326de8bd14338
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-08-20 11:09:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const instruction = document.getElementById('instruction')
 
 window.addEventListener('load', () => {
     scanner.classList.add('animate-radar')
+    board.classList.add('loading')
     document.body.style.cursor = "wait";
     instruction.style.transition = "all 0.5s"
     instruction.style.color = "#191f22"
@@ -12,12 +13,12 @@ window.addEventListener('load', () => {
 board.addEventListener("animationend",  transitionAnim)
 
 function transitionAnim(){
-    setTimeout(()=>{
-        board.style.display = "none"
+        board.classList.remove('loading')
+        board.classList.add('loaded')
         document.body.style.cursor = "default"
+
         attribution.style.color = "#fff"
-        attribution.style.transition = "all 2s"
+        attribution.style.transition = "all ease-in 2s"
         instruction.style.color = "#fff"
-        instruction.style.transition = "all 2s"
-    },2800)    
+        instruction.style.transition = "all 2s ease-in 2s" 
 }
