6ee93e33dc27e903b9c0a85bd9f8f9eca01ede48
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-08-22 16:05:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,13 @@
 const board = document.getElementById('board');
-const grid = document.getElementById('grid')
 const instruction = document.getElementById('instruction')
 const btn = document.getElementById('btn')
-const radar = document.getElementById('radar')
-const summary = document.getElementById('learning-summary')
-
-function generateScale(){
-    let randomScale = ((Math.random()) * (0.7-0)+1).toFixed(2);
-    return randomScale
-}
-
-function createElements(n, parent, desc){
-    let count = 1
-
-    while (count <= n){
-        const el = document.createElement('div')
-        el.classList.add(desc)
-
-        if (desc =='concentric__circle'){
-            let size = count* 20
-            el.style.width=`${size}%`;
-            el.style.height=`${size}%`;
-        } else if (desc == 'radial'){
-            el.style.cssText = `--i:${count}`;
-        } else if (desc == 'radar__obj'){
-            let blobScale = generateScale()
-            let revCount = 11-count
-            el.classList.add(`radar__obj--${count}`)
-            el.setAttribute('data-line', revCount)
-            el.style.animationDelay = `${count+1}s`
-            el.style.transform = `scale(${blobScale})`;     
-        }    
-        parent.appendChild(el)        
-        count++
-    }
-}
-
-function createScanner (){
-    const scanner = document.createElement('div')
-    scanner.setAttribute("id", "scanner")
-    grid.appendChild(scanner)
-}
-
-createElements(4, grid, 'concentric__circle')
-createElements(6, grid, 'radial')
-createScanner()
-createElements(10, grid, 'radar__obj')
-
 const scanner = document.getElementById('scanner')
+const shrinkBoard = document.querySelector('.shrink-board')
 const radarObj = document.querySelectorAll('.radar__obj')
 
 btn.addEventListener('click', startAnim)
 
-btn.addEventListener('mouseover', function(){
-    this.style.opacity="0.5"
-})
-
-btn.addEventListener('mouseout', function(){
-    if(window.getComputedStyle(summary).opacity=='1'){
-        this.style.opacity="1"
-    }
-})
-
- function startAnim(e){
+function startAnim(e){
     document.body.style.cursor = "wait";
     this.classList.add('btn-disabled')
     this.classList.remove('btn-enabled')
@@ -70,21 +15,17 @@ btn.addEventListener('mouseout', function(){
     scanner.classList.add('start-scanner')
     radarObj.forEach(obj =>{
         obj.classList.add('start-glow')
-    })    
-        if(window.getComputedStyle(summary).opacity=='1'){
-            summary.style.opacity='0'
-        }
-    }
+    })
+}
 
-const finalBlip = document.querySelector('.radar__obj--5')
-finalBlip.addEventListener("animationend", objectIsScanned)
+board.addEventListener("animationend",  transitionAnim)
 
-function objectIsScanned(){
-    document.body.style.cursor = "default"
-    btn.classList.remove('btn-disabled')
-    btn.classList.add('btn-enabled')
-    if(window.getComputedStyle(summary).opacity=='0'){
-        summary.style.opacity='1'
+function transitionAnim(event){
+    if(event.animationName ==='blink') {
+        document.body.style.cursor = "default"
+        btn.classList.remove('btn-disabled')
+        btn.classList.add('btn-enabled')
+        board.classList.add('shrink-board')
         scanner.classList.remove('start-scanner')
         radarObj.forEach(obj =>{
         obj.classList.remove('start-glow')
